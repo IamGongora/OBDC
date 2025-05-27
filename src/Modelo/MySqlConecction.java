629b0a4a0a07ad412eb64cf4714f8018caa7ca5d
Jhon M-L <jhonalexme356@gmail.com>
2025-05-27 00:09:44 -0500

    Conectamos la BD en el archivo MySqlConecction

---
+++ src/Modelo/MySqlConecction.java
@@ -0,0 +1,31 @@
+package Modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MySqlConecction {
+
+    private static final String ruta = "jdbc:mysql://localhost:3306/bdpfinal";
+    private static final String usuario = "root";
+    private static final String contrasena = "";
+
+    public static Connection conectar() {
+        Connection conexion = null;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conexion = DriverManager.getConnection(ruta, usuario, contrasena);
+            System.out.println("Conexion exitosa");
+        } catch (ClassNotFoundException e) {
+            System.err.println("No se encontro el driver.");
+        } catch (SQLException e) {
+            System.err.println("Error en la conexion a la BD.");
+            e.printStackTrace();
+        }
+        
+        return conexion;
+    }
+}
